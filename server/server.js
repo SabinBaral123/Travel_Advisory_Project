8ecd9efa4b61dfeb167a86673a94a5b5d48f3f0a
SabinBaral <sabinbaral7777@gmail.com>
2025-02-10 03:49:58 -0500

    Removed CLI stuffs to cli.js  make the server more cleaner

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
 import vars from "./modules/vars.js";
 import * as db from "./modules/db.js";
-import * as setup from "./modules/setup.js";
-
-let client = null;
-
+import * as api from "./modules/api.js"
+import * as cli from "./modules/cli.js"
 try {
-
-    client = await db.initDatabase(vars);
-    await setup.refreshDatabase(client, vars);
+    let client = await db.initDatabase(vars);
+    await cli.processArgs(client, vars);
+    api.configure(client,vars);
+    api.startServer(vars.PORT);
+    //console.warn("!");
 
 }
 catch (e) {
     console.error(`${e}`);
-}
-finally {
-    await client?.close();
 }
