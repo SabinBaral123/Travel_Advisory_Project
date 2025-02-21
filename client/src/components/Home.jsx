c02676d6677455d80739146b4a4f4c2ba91b40f1
SabinBaral <sabinbaral7777@gmail.com>
2025-02-21 08:00:07 -0500

    Frontend fetching the data from the server and giving details on snackbar

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -8,17 +8,42 @@ import {
   TextField,
   Button,
   Paper,
+  Snackbar,
 } from "@mui/material";
 
 import "../App.css";
 import { useState } from "react";
 
+
+
 const Home = () => {
   const [userInput, setUserInput] = useState("");
-
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarText, setSnackbarText] = useState('');
   const findAlert = () => {
     alert(`This will be a call to /api/users/:${userInput}`);
   };
+  const findName = async email => {
+    let message = 'No results found';
+    try {
+        let response = await fetch(`http://localhost:9000/api/users?email=${email}`);
+        let result = await response.json();
+        console.log(result);
+        if (result.length > 1) {
+            message = `${result.length} users found`;
+        }
+        else if (result.length > 0) {
+            message = `User ${result[0].name} found`;
+        }
+    }
+    catch (e) {
+        console.warn(`${e}`);
+        message = `Search failed`;
+    }
+
+    setSnackbarOpen(true);
+    setSnackbarText(message);
+}
   return (
     <>
       <AppBar position = "sticky">
@@ -41,12 +66,17 @@ const Home = () => {
             variant="contained"
             className="buttonFind"
             sx={{ marginTop: "2em" }}
-            onClick={findAlert}
-          >
+            onClick={() => findName(userInput)}          >
             FIND
           </Button>
         </CardContent>
       </Paper>
+      <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={5000}
+            onClose={() => setSnackbarOpen(false)}
+            message={snackbarText}
+        />
     </>
   );
 };
